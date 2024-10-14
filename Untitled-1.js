b2662597b2f8a89963be3e2256b7fddf249bf6da
FlyCode <161161655+FlyCode30@users.noreply.github.com>
2024-10-14 09:43:37 -0400

    initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+// yatzy javascript
+
+let startSet = [
+        'images/side_1.png',
+        'images/side_2.png',
+        'images/side_3.png',
+        'images/side_4.png',
+        'images/side_5.png',
+        'images/side_6.png'
+    ];
+
+    
