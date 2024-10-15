fdc6cc2007feaa17e8495268989ce688546ee087
FlyCode <161161655+FlyCode30@users.noreply.github.com>
2024-10-14 20:42:54 -0400

    click and roll functionality

---
+++ scriptMain.js
@@ -0,0 +1,136 @@
+// load scoreboard
+
+function loadScoreBoard(page) {
+    fetch(page)
+        .then(Response => Response.text())
+        .then(data => {
+            const scoreBoard = document.getElementById('scoreTable');
+            scoreBoard.innerHTML = data;
+            scoreBoard.classList.add('show');
+        })
+}
+
+window.onload = function () {
+    loadScoreBoard('scoreAreaYatzy.html');
+}
+
+// constants
+
+const newRollButton = document.getElementById('newRoll');
+
+const reRollButton = document.getElementById('reRoll');
+
+const selectImage = document.getElementById
+
+newRollButton.addEventListener('click', getDice);
+
+reRollButton.addEventListener('click', reRoll);
+
+// yatzy javascript
+
+let rollValue;
+
+let kindValue1;
+
+let kindValue2;
+
+
+
+// loop throgh each dice and add
+
+
+// dice roll
+
+function getDiceNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+} // Math.random gives a number that is greater >= 0 and < 1. Multiply by 6 and add 1 gives you 1-6 because it can't be 1
+
+// switch 
+
+// function getDiceImage(diceRoll) {
+    
+//     console.log(`Generating image for dice number: ${diceRoll}`);
+
+//     switch (diceRoll) {
+//         case 1:
+//             return 'Side1.png' + console.log(`Image retrieved for ${diceRoll}`);
+//             break;
+//         case 2:
+//             return 'Side2.png';
+//             break;
+//         case 3:
+//             return 'Side3.png';
+//             break;
+//         case 4:
+//             return 'Side4.png';
+//             break;
+//         case 5:
+//             return'Side5.png';
+//             break;
+//         case 6:
+//             return'Side6.png';
+//             break;
+//         default:
+//             console.log('Invalid dice roll');
+//             return '';
+//     }
+// }
+
+
+
+function getDice() {
+
+    for (let i = 1; i <= 5; i++) {
+        const diceContainer = document.getElementById(`dice${i}`); // get the container that holds the image
+        console.log(`Dice container for roll ${i}`, diceContainer);
+
+        diceContainer.style.display = "flex"; // make visible by making the images flex
+
+        const diceNumber = getDiceNumber(); // get a random number
+        console.log(`Dice ${i} rolled: ${diceNumber}`);
+
+        const imgPath = `side${diceNumber}.png`;
+        console.log(`Generated image path: ${imgPath}`);
+
+        diceContainer.src = imgPath;
+    }
+}
+
+function selectDiceClick(diceNumber) {
+
+    const clickedDice = document.getElementById(`dice${diceNumber}`);
+
+    if (clickedDice.style.display === "none") {
+        clickedDice.style.display = "flex";
+    } else {
+        clickedDice.style.display = "none";
+
+        diceContainer = document.getElementById(`choice${diceNumber}`);
+        const imgPath = 'side'
+    }
+}
+
+function reRoll() {
+
+    
+    for (let i = 1; i <= 5; i++) {
+        let diceContainer = document.getElementById(`dice${i}`); // get the container that holds the image
+        console.log(`Dice container for roll ${i}`, diceContainer);
+
+        if (diceContainer.style.display === "flex") {
+            const diceNumber = getDiceNumber(); // get a random number
+            console.log(`Dice ${i} rolled: ${diceNumber}`);
+    
+            const imgPath = `side${diceNumber}.png`;
+            console.log(`Generated image path: ${imgPath}`);
+    
+            diceContainer.src = imgPath;
+        }
+
+    }
+}
+
+
+
+// does a new full roll
+
